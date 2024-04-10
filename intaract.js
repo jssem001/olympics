9abc7f432e287c40bd7fefac2d09b86bf32988d1
Joshua Ssemwanga <jssem001@plattsburgh.edu>
2024-04-10 16:52:17 +0300

    fetches added

--- intaract.js
+++ intaract.js
@@ -1,20 +1,21 @@
 // Javascript file
 
-//PARIS 2024 coundown clock with help from w3school.com
-let now; let later; let days; let hours; let minutes; let seconds 
-// Set the date we're counting down to
-var countDownDate = new Date("Jul 26, 2024 20:24:00").getTime();
-// Update the count down every 1 second
-var x = setInterval(() => {    
-// Get today's date and time
-now = new Date().getTime();   
-// Find the distance between now and the count down date
-later = countDownDate - now;    
-// Time calculations for days, hours, minutes and seconds
-days = Math.floor(later / (1000 * 60 * 60 * 24)); hours = Math.floor((later % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-minutes = Math.floor((later % (1000 * 60 * 60)) / (1000 * 60)); seconds = Math.floor((later % (1000 * 60)) / 1000);  
-// Display the result in the element with id="paris"
-document.getElementById("paris").innerHTML = "PARIS 2024 "+ days + "d " + hours + "h "+ minutes + "m " + seconds + "s ";}, 1000);
+//GET Request for articles
+fetch('http://localhost:3000/articles')
+ .then(response => {
+    return response.json()
+})
+ .then(data => {
+    for(let x of data) {
+        document.getElementById('latestPost').innerHTML += 
+        `<section id="postCon1">
+        <p class="postHeadline">${x.headline}</p>
+        <img id=postCon3Img src=${x.image} alt="hockey">
+        <p>${x.subhead}</p>
+        </section>`
+        }    
+})
+
 
 //GET request for likes
 fetch('http://localhost:3000/articles/1')
@@ -25,9 +26,7 @@ fetch('http://localhost:3000/articles/1')
     let likeCount = data.likes
     document.getElementById('like-count').innerHTML = `${likeCount}`    
 })
- .catch(error => {
-    console.log('An Error occured: ', error)
-})
+
 
 //GET request to retrieve comments
 fetch('http://localhost:3000/comments')
@@ -36,24 +35,22 @@ fetch('http://localhost:3000/comments')
 })
  .then(data => {
     for(let c of data) {
-    //document.getElementById('comments-list').innerHTML += `<li>${c.content}  <span id=${c.id} class="delete-button" style='font-size: 10px'> &#10060;</span></li>`
-    document.getElementById('comments-list').innerHTML += `<li id='${c.id}' onclick='deletefunction(this,${c.id})'>${c.content}  <span style='font-size: 10px'> &#10060;</span></li>`
+    document.getElementById('comments-list').innerHTML += 
+    `<li id='${c.id}' onclick='deletefunction(this,${c.id})'>${c.content}  <span style='font-size: 10px'> &#10060;</span></li>`
     }
 })
- .catch(error => {
-    console.log('An Error occured: ', error)
-})
 
-//addding a comment
-//POST request for comments 
-document.getElementById("comment-form").addEventListener('submit',function(event){
+
+//addding a comment with POST request 
+document.getElementById("comment-form").addEventListener("submit",function(event){
     event.preventDefault()
     let comment = document.getElementById('comment').value 
-
+    let newid = Math.floor(Math.random() * 1000)
     console.log(comment)
     fetch('http://localhost:3000/comments', {
-        method: 'POST',
+        method: "POST",
         body: JSON.stringify({
+            id: `c${newid}`,
             imageId: 1,
             content: comment,
         }),
@@ -62,8 +59,7 @@ document.getElementById("comment-form").addEventListener('submit',function(event
         },
     })
     .then((data) => data.json())
-    .then((response) => 
-     console.log('comment added'));
+    .then((comment) => console.log(comment));
 
 })
 
@@ -97,22 +93,31 @@ document.getElementById("like-button").addEventListener('click',function(event){
         });
 })
 
-//Update Comment
+//New Post
+document.getElementById("newPost").addEventListener("submit",function(event){
+    event.preventDefault()
+    const newheadline = document.getElementById('newHeadline').value
+    const newimage = document.getElementById('image-input').value
+    const newsubhead = document.getElementById('subhead').value
 
-// fetch('https://jsonplaceholder.typicode.com/posts/1', {
-//   method: 'PUT',
-//   body: JSON.stringify({
-//     id: 1,
-//     title: 'foo',
-//     body: 'bar',
-//     userId: 1,
-//   }),
-//   headers: {
-//     'Content-type': 'application/json; charset=UTF-8',
-//   },
-// })
-//   .then((response) => response.json())
-//   .then((json) => console.log(json));
+    console.log(newheadline,newimage,newsubhead)
+    fetch('http://localhost:3000/articles', {
+    method: "POST",
+    body: JSON.stringify({
+        headline: newheadline,
+        image: newimage,
+        subhead: newsubhead,
+        likes: 0
+    }),
+    headers: {
+        'Content-type': 'application/json'
+    },
+    })
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+
+})
+
 
 //Deleting a comment
 function deletefunction(el,id){
@@ -131,3 +136,26 @@ function deletefunction(el,id){
 
 
 }
+
+
+
+
+
+//***BONUS FEATURE*** uncomment to use
+
+
+// //PARIS 2024 coundown clock with help from w3school.com
+// let now; let later; let days; let hours; let minutes; let seconds 
+// // Set the date we're counting down to
+// var countDownDate = new Date("Jul 26, 2024 20:24:00").getTime();
+// // Update the count down every 1 second
+// var x = setInterval(() => {    
+// // Get today's date and time
+// now = new Date().getTime();   
+// // Find the distance between now and the count down date
+// later = countDownDate - now;    
+// // Time calculations for days, hours, minutes and seconds
+// days = Math.floor(later / (1000 * 60 * 60 * 24)); hours = Math.floor((later % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+// minutes = Math.floor((later % (1000 * 60 * 60)) / (1000 * 60)); seconds = Math.floor((later % (1000 * 60)) / 1000);  
+// // Display the result in the element with id="paris"
+// document.getElementById("paris").innerHTML = "PARIS 2024 "+ days + "d " + hours + "h "+ minutes + "m " + seconds + "s ";}, 1000);
